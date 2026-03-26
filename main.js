cd50ee0b36e7592a9fbef21ac16f2fb1c736bf76
ivdev04 <101958046+Ivan04dev@users.noreply.github.com>
2026-03-26 12:02:13 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+-- Aquí se importan las dependencias como Bootstrap y Bootstrap icons:
+
+import './assets/main.css'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap'
+ 
+import { createApp } from 'vue'
+import App from './App.vue'
+ 
+createApp(App).mount('#app')
